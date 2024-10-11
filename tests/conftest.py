d77700667c9e0ca70923d1bb40da680cdb0b4897
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-10-11 17:54:24 +0200

    add jwt auth tests and related fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import datetime
 
 from my_app.services.user_service import UserService  # Assure-toi d'importer UserService correctement
 
@@ -17,3 +18,30 @@ def mock_session(mocker):
 def user_service(mock_session):
     """Fixture for creating an UserService instance with a mocked session"""
     return UserService(session=mock_session)
+
+
+@pytest.fixture
+def mock_time(monkeypatch):
+    """Fixture to freeze time !"""
+    fake_time = datetime.datetime(2023, 1, 2, 20, 0, 0)
+
+    class MockDateTime:
+        @classmethod
+        def now(cls, tz=None):
+            if tz:
+                return mock_time.astimezone(tz)  # utilise la timezone si indiqué
+            return mock_time
+
+    monkeypatch.setattr(datetime, "datetime", MockDateTime)
+    return fake_time
+
+
+@pytest.fixture
+def mock_user(monkeypatch):
+    """Fixture to mock a user"""
+    class MockUser:
+        def __init__(self, user_id):
+            self.id = user_id
+
+    # TODO : a revoir une fois les couches d'abstraction ajoutées (uniquement l'id pour l'instant)
+    return MockUser(user_id=8)
