5335d24da43380c7b643959958c32da014705377
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-10-16 21:20:02 +0200

    script refacto create AppAdmin user

--- scripts/2_create_app_admin.py
+++ scripts/2_create_app_admin.py
@@ -1,8 +1,11 @@
 import os
 import re
+import sys
+
 from getpass import getpass
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, URL, text
+from sqlalchemy.orm import sessionmaker
 
 # Pour activer l'option echo de SQLAlchemy (affichage du SQL, très verbeux)
 ECHO = False
@@ -23,7 +26,7 @@ class AppUserCreator:
         self.SSL_MODE = os.getenv("SSL_MODE")
 
     def get_engine(self, username, password):
-        """create the SQLAlchemy engine (with psycopg2)"""
+        """create the SQLAlchemy engine"""
         url_object = URL.create(
             "postgresql+psycopg2",
             username=username,
@@ -62,51 +65,81 @@ class AppUserCreator:
 
             return username, password
 
-    def create_user(self, superuser_name, superuser_password, new_user_username, new_user_password):
-        """Create an user"""
-        self.get_engine(superuser_name, superuser_password)
+    def check_user_exists(self, username):
+        """Check if a PostgreSQL user exists"""
         try:
-            # Connexion à la base pour créer l'utilisateur le nouvel utilisateur
             with self.engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
-                query = f"CREATE USER \"{new_user_username}\" WITH PASSWORD '{new_user_password}';"
-                conn.execute(text(query))
-                print(f"Création du compte : '{new_user_username}' => OK")
+                result = conn.execute(text(f"SELECT 1 FROM pg_roles WHERE rolname = '{username}';")).fetchone()
+                if result:
+                    return True
+                else:
+                    return False
+        except Exception as e:
+            print(f"Erreur lors de la vérification de l'utilisateur '{username}' : {e}")
+            return False
+
+    def verify_user_password(self, username, password):
+        """Verify if the username and password are correct (try a connection)"""
+        try:
+            # Tentative de connexion avec le nom d'utilisateur et le mot de passe fournis
+            test_url = URL.create(
+                "postgresql+pg8000",
+                username=username,
+                password=password,
+                host=self.DATABASE_HOST,
+                port=self.DATABASE_PORT,
+                database=self.DATABASE_NAME,
+            )
+            test_engine = create_engine(test_url, echo=ECHO, future=True)
+            # test de connexion basique
+            with test_engine.connect() as conn:
+                conn.execute(text("SELECT 1"))
+                return True
+        except Exception as e:
+            print(f"Erreur de connexion '{username}' : {e}")
+            return False
+
+    def create_user(self, session, new_user_username, new_user_password):
+        """Create a user"""
+        try:
+            session.execute(text(f"CREATE USER \"{new_user_username}\" WITH PASSWORD '{new_user_password}';"))
+            print(f"Création du compte : '{new_user_username}' => OK")
+
         except Exception as e:
             print(f"Erreur lors de la création du compte '{new_user_username}' : {e}")
 
-    def grant_privileges(self, username):
+    def grant_privileges(self, session, username):
         """Grant privileges to the user account and restricted access to schema"""
         try:
-            with self.engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
-                # accede uniquement au schema, sur des opérations CRUD
-                conn.execute(text(
-                    f"GRANT USAGE ON SCHEMA {self.DATABASE_SCHEMA} TO {username};"
-                    ))
-                # Grant privileges on all existing tables in the schema
-                conn.execute(text(
-                    f"GRANT SELECT, INSERT, UPDATE, DELETE"
-                    f"ON ALL TABLES IN SCHEMA {self.DATABASE_SCHEMA} TO {username};"
-                    ))
+            # accede uniquement au schema, sur des opérations CRUD
+            session.execute(text(
+                f"GRANT USAGE ON SCHEMA {self.DATABASE_SCHEMA} TO {username};"
+                ))
+            # donne tous les privilèges sur toutes les tables du schema
+            session.execute(text(
+                f"GRANT SELECT, INSERT, UPDATE, DELETE"
+                f" ON ALL TABLES IN SCHEMA {self.DATABASE_SCHEMA} TO {username};"
+                ))
 
-                # Grant privileges on future tables in the schema
-                conn.execute(text(
-                    f"ALTER DEFAULT PRIVILEGES IN SCHEMA {self.DATABASE_SCHEMA}"
-                    f"GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO {username};"
-                    ))
+            # donne tous les privilèges sur toutes les tables FUTURES du schema ("ALTER DEFAULT")
+            session.execute(text(
+                f"ALTER DEFAULT PRIVILEGES IN SCHEMA {self.DATABASE_SCHEMA}"
+                f"GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO {username};"
+                ))
 
-                # Grant usage and select on sequences (but no modification rights)
-                conn.execute(text(
-                    f"GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA {self.DATABASE_SCHEMA} TO {username};"
-                    ))
-                conn.execute(text(
-                    f"ALTER DEFAULT PRIVILEGES IN SCHEMA {self.DATABASE_SCHEMA}"
-                    f"GRANT USAGE, SELECT ON SEQUENCES TO {username};"
-                    ))
+            # on donne l'acces d'usage et de select sur toutes les sequences (mais sans pouvoir les modifier)
+            session.execute(text(
+                f"GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA {self.DATABASE_SCHEMA} TO {username};"
+                ))
+            session.execute(text(
+                f"ALTER DEFAULT PRIVILEGES IN SCHEMA {self.DATABASE_SCHEMA}"
+                f"GRANT USAGE, SELECT ON SEQUENCES TO {username};"
+                ))
 
-                # revoque les privileges sur les autres schemas (que public ici)
-                conn.execute(text(f"REVOKE ALL ON SCHEMA public FROM {username};"))
+            # revoque les privileges sur les autres schemas (que public dans notre cas d'usage ici)
+            session.execute(text(f"REVOKE ALL ON SCHEMA public FROM {username};"))
 
-                print(f'Les privilèges ont été appliqués pour "{username}" sur le schéma "{self.DATABASE_SCHEMA}"')
+            print(f'Les privilèges ont été appliqués pour "{username}" sur le schéma "{self.DATABASE_SCHEMA}"')
         except Exception as e:
             print(f"Erreur d'attribution des privilèges pour '{username}' : {e}")
 
@@ -118,11 +151,37 @@ if __name__ == "__main__":
     # Demande le nom et le mot de passe du superuser
     superuser_name, superuser_password = app_user_creator.request_superuser_access()
 
+    # Création d'un engine avec le superuser
+    app_user_creator.get_engine(superuser_name, superuser_password)
+    # autocommit activé, a voir si pertinent (pb de rollback?)
+    Session = sessionmaker(bind=app_user_creator.engine, autocommit=True)
+    session = Session()
+
     # Demande des informations pour créer l'utilisateur
     new_user_username, new_user_password = app_user_creator.request_user_infos()
 
-    # Création du compte
-    app_user_creator.create_user(superuser_name, superuser_password, new_user_username, new_user_password)
+    # # TODO to move
+    # self.get_engine(superuser_name, superuser_password)
 
-    # Attribution des privilèges sur le schéma
-    app_user_creator.grant_privileges(new_user_username)
+    # on regarde si l'utilisateur indiqué existe déjà
+    if app_user_creator.check_user_exists(new_user_username):
+        print(f"L'utilisateur '{new_user_username}' existe déjà.")
+
+        # si le mdp indiqué est correct....
+        if not app_user_creator.verify_user_password(new_user_username, new_user_password):
+            print("Le mot de passe indiqué ne correspond pas a l'utilisateur existant.\
+                   Merci de créer un autre utilisateur.")
+            sys.exit(1)  # arret du script
+
+        # ... on demande confirmation pour la modification des privilèges
+        confirmation = input(f"Etes vous sur de vouloir modifier les privilèges de l'utilisateur '{new_user_username}' ? (y/n): ")
+
+        if confirmation.lower() == 'y':
+            app_user_creator.grant_privileges(new_user_username)
+            print(f"Privilèges modifiés pour '{new_user_username}'.")
+        else:
+            print("Modification des privilèges annulée.")
+    else:
+        # si le compte n'existe pas, création du compte et ajout des privilèges
+        app_user_creator.create_user(session, new_user_username, new_user_password)
+        app_user_creator.grant_privileges(session, new_user_username)
