dd2e15722c338d0e7dd391ec96118076b8bac463
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-10-21 21:11:09 +0200

    add customer dependencies

--- src/my_app/dependencies.py
+++ src/my_app/dependencies.py
@@ -1,9 +1,14 @@
 from sqlalchemy.orm import Session
 
+# main
 from .repositories.user_repository import UserRepository
 from .services.user_service import UserService
 from .services.token_service import TokenManager
 from .controllers.main_controller import MainController
+# customer
+from .repositories.customer_repository import CustomerRepository
+from .services.customer_service import CustomerService
+from .controllers.customer_controller import CustomerController
 
 
 def get_user_service(session: Session):
@@ -16,6 +21,12 @@ def get_user_service(session: Session):
     return UserService(session, user_repository)
 
 
+def get_customer_service(session: Session):
+    """create an instance of CustomerService"""
+    customer_repository = CustomerRepository(session)
+    return CustomerService(session, customer_repository)
+
+
 def get_token_manager() -> TokenManager:
     """create an instance of TokenManager"""
     return TokenManager()
@@ -26,3 +37,9 @@ def init_main_controller(session: Session):
     user_service = get_user_service(session)
     token_manager = get_token_manager()
     return MainController(user_service, token_manager)
+
+
+def init_customer_controller(session: Session):
+    """Create an instance of CustomerController"""
+    customer_service = get_customer_service(session)
+    return CustomerController(customer_service)
