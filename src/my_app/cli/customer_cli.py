8a846159afb6fdb5dd8bc330c957bb0d09cbf053
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-10-26 22:48:05 +0200

    fix comma

--- src/my_app/cli/customer_cli.py
+++ src/my_app/cli/customer_cli.py
@@ -44,9 +44,9 @@ def display_customer_table(customers):
 @click.pass_context
 def customer(ctx):
     """Group of commands to manage customers"""
-    session = ctx.obj['session']
+    session = ctx.obj["session"]
     # init de CustomerController et stockage dans le contexte
-    ctx.obj['customer_controller'] = init_customer_controller(session)
+    ctx.obj["customer_controller"] = init_customer_controller(session)
 
 
 @customer.command(help="List all customers")
@@ -55,8 +55,8 @@ def customer(ctx):
 @handle_exceptions
 def list(ctx):
     """Display all customers"""
-    customer_controller = ctx.obj.get('customer_controller')
-    current_user = ctx.obj['authenticated_user']
+    customer_controller = ctx.obj.get("customer_controller")
+    current_user = ctx.obj["authenticated_user"]
     customers = customer_controller.list(current_user)
 
     if not customers:
@@ -69,13 +69,13 @@ def list(ctx):
 
 @customer.command(help="Get a customer by ID")
 @requires_auth
-@click.argument('customer_id', type=int, required=True)
+@click.argument("customer_id", type=int, required=True)
 @click.pass_context
 @handle_exceptions
 def get(ctx, customer_id):
     """Retrieve and display a customer by his ID"""
-    customer_controller = ctx.obj.get('customer_controller')
-    current_user = ctx.obj['authenticated_user']
+    customer_controller = ctx.obj.get("customer_controller")
+    current_user = ctx.obj["authenticated_user"]
     customer = customer_controller.get(customer_id, current_user)
 
     if not customer:
@@ -88,16 +88,16 @@ def get(ctx, customer_id):
 
 @customer.command(help="Add a new customer")
 @requires_auth
-@click.argument('company_name', metavar='<company name>', required=True)
-@click.argument('full_name', metavar='<full name>', required=True)
-@click.option('--email', metavar='<email>', required=False, help="customer email address")
-@click.option('--phone', metavar='<phone number>', required=False, help="customer phone number")
+@click.argument("company_name", metavar="<company name>", required=True)
+@click.argument("full_name", metavar="<full name>", required=True)
+@click.option("--email", metavar="<email>", required=False, help="customer email address")
+@click.option("--phone", metavar="<phone number>", required=False, help="customer phone number")
 @click.pass_context
 @handle_exceptions
 def add(ctx, company_name, full_name, email, phone):
     """Add a new customer"""
-    current_user = ctx.obj['authenticated_user']
-    customer_controller = ctx.obj['customer_controller']
+    current_user = ctx.obj["authenticated_user"]
+    customer_controller = ctx.obj["customer_controller"]
 
     customer_data = {
         "company_name": company_name,
@@ -112,28 +112,28 @@ def add(ctx, company_name, full_name, email, phone):
 
 @customer.command(help="Update an existing customer")
 @requires_auth
-@click.argument('customer_id', metavar='<customer id>', type=int, required=True)
-@click.option('--company_name', metavar='<company name>', required=False, help="company name")
-@click.option('--full_name', metavar='<full name>', required=False, help="customer full name")
-@click.option('--email', metavar='<email>', required=False, help="customer email address")
-@click.option('--phone', metavar='<phone number>', required=False, help="customer phone number")
-@click.option('--sales_id', metavar='<sales contact ID>',
+@click.argument("customer_id", metavar="<customer id>", type=int, required=True)
+@click.option("--company_name", metavar="<company name>", required=False, help="company name")
+@click.option("--full_name", metavar="<full name>", required=False, help="customer full name")
+@click.option("--email", metavar="<email>", required=False, help="customer email address")
+@click.option("--phone", metavar="<phone number>", required=False, help="customer phone number")
+@click.option("--sales_id", metavar="<sales contact ID>",
               type=int, required=False, help="ID of the sales contact")
 @click.pass_context
 @handle_exceptions
 def update(ctx, customer_id, company_name, full_name, email, phone, sales_id):
     """Update an existing customer"""
-    current_user = ctx.obj['authenticated_user']
-    customer_controller = ctx.obj['customer_controller']
+    current_user = ctx.obj["authenticated_user"]
+    customer_controller = ctx.obj["customer_controller"]
 
     # on ajoute les données au dictionnaire
     update_data = {
         key: value for key, value in {
-            'company_name': company_name,
-            'full_name': full_name,
-            'email': email,
-            'phone_number': phone,
-            'contact_sales_id': sales_id
+            "company_name": company_name,
+            "full_name": full_name,
+            "email": email,
+            "phone_number": phone,
+            "contact_sales_id": sales_id
         }.items() if value is not None
     }
 
@@ -147,12 +147,12 @@ def update(ctx, customer_id, company_name, full_name, email, phone, sales_id):
 
 @customer.command(help="Delete a customer")
 @requires_auth
-@click.argument('customer_id', metavar='<customer id>', required=True)
+@click.argument("customer_id", metavar="<customer id>", required=True)
 @click.pass_context
 @handle_exceptions
 def delete(ctx, customer_id):
     """Delete a customer"""
-    current_user = ctx.obj['authenticated_user']
-    customer_controller = ctx.obj['customer_controller']
+    current_user = ctx.obj["authenticated_user"]
+    customer_controller = ctx.obj["customer_controller"]
     customer_controller.delete(current_user, customer_id)
     console.print(f"Customer {customer_id} deleted!")
