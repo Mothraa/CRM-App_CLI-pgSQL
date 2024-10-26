01648cdbef6b5825e18f227f1d42a17070d04c7b
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-10-26 14:56:56 +0200

    add get_all and remove permission check in service

--- src/my_app/services/user_service.py
+++ src/my_app/services/user_service.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session
 from my_app.repositories.user_repository import UserRepository
 from my_app.models import User
 from my_app.schemas.user_schemas import UserAuthSchema, UserAddSchema, UserUpdateSchema
-from my_app.permissions import has_permission  # , has_any_permission, has_all_permission
 from my_app.exceptions import UserNotFoundError, InvalidPasswordError
 
 
@@ -27,39 +26,39 @@ class UserService:
         return self.ph.hash(password)
 
     def verify_password(self, hashed_password: str, plain_password: str) -> bool:
-        """For verify a password and his hash, return a boolean"""
+        """For verify a password and its hash, return a boolean"""
         try:
             return self.ph.verify(hashed_password, plain_password)
         except VerifyMismatchError:
             return False
 
-    def get_user_by_id(self, user_id: int):
+    def get_all(self) -> list[User]:
+        """Retrieve all users"""
+        return self.user_repository.get_all()
+
+    def get_by_id(self, user_id: int):
         """Retrieve a User by his ID"""
         user = self.user_repository.get_by_id(user_id)  # Appel au repository
         if not user:
             raise UserNotFoundError(f"Pas d'utilisateur avec l'ID : {user_id}")
         return user
 
-    def authenticate_user(self, user_email, password):
-        """Authenticate user, return User instance if OK, or None"""
+    def authenticate_user(self, user_email: str, password: str):
+        """Authenticate user, return User instance if OK, or raise an error"""
         # On valide le format des données d'entrée avec Pydantic
         UserAuthSchema(email=user_email, password=password)
 
         user = self.user_repository.get_by_email(user_email)
-        # user = self.user_repository.get_by_email(self.session, user_email)
         if user is None:
             raise UserNotFoundError(f"Pas d'utilisateur avec l'email : {user_email}")
 
         if not self.verify_password(user.password_hash, password):
             raise InvalidPasswordError("Password invalid")
 
-        return user  # on retourne l'utilisateur si le mot de passe est OK
+        return user
 
-    def add_user(self, user_data: dict, current_user: User) -> User:
+    def add(self, user_data: dict) -> User:
         """Method to add a new user. Hash the given password"""
-        # on regarde si l'utilisateur courant a la permission de créer un compte.
-        has_permission(current_user, 'add-user')  # lève une exception si NOK
-
         # On valide les données d'entrée avec Pydantic
         user_add = UserAddSchema(**user_data)
 
@@ -76,27 +75,19 @@ class UserService:
         user_add_dict["password_hash"] = hashed_password
         del user_add_dict["password"]
 
-        # creation d'un utilisateur via le repository
+        # création d'un utilisateur via le repository
         new_user = self.user_repository.add(User(**user_add_dict))
         return new_user
 
-    def update_user(self, user_id: int, update_data: dict, current_user: User) -> User:
-        """Method to update an existing user, need "update-user" permission
-        param :
-            user_id : the id of the user to update
-            update_data : ...
-            current_user : active authentified current user
-        """
-        # on regarde si l'utilisateur courant a la permission de mettre à jour un compte.
-        has_permission(current_user, "update-user")  # lève une exception si NOK
-
+    def update(self, user_id: int, update_data: dict) -> User:
+        """Method to update an existing user"""
         # On valide les données d'entrée avec Pydantic
         user_update = UserUpdateSchema(**update_data)
 
         # On récupère l'utilisateur à mettre à jour
         user_to_update = self.user_repository.get_by_id(user_id)
 
-        # Si un nouveau password est fourni, on regarde les permissions et on le hache
+        # Si un nouveau password est fourni, on le hache
         if "password" in update_data:
             hashed_password = self.hash_password(user_update.password)
             update_data["password_hash"] = hashed_password
@@ -111,15 +102,9 @@ class UserService:
         updated_user = self.user_repository.update(user_to_update)
         return updated_user
 
-    def delete_user(self, user_id: int, current_user: User):
+    def delete(self, user_id: int):
         """Delete a User by his ID"""
-        # on regarde si l'utilisateur courant a la permission de supprimer un compte.
-        has_permission(current_user, "delete-user")  # lève une exception si NOK
         # On récupère l'utilisateur à supprimer
-        user_to_delete = self.user_repository.get_by_id(user_id)
-
-        # ...rien a valider du coté de pydantic...
-
         user_to_delete = self.user_repository.get_by_id(user_id)
         self.user_repository.delete(user_to_delete)
         # on ne retourne rien, par cohérence avec le repository et pour ne pas exposer les données
