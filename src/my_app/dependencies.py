5767b4679e5e95d4339dab134d3d325e12ecc022
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-10-26 15:01:43 +0200

    add user, event and contract dependencies

--- src/my_app/dependencies.py
+++ src/my_app/dependencies.py
@@ -1,14 +1,24 @@
 from sqlalchemy.orm import Session
 
 # main
-from .repositories.user_repository import UserRepository
-from .services.user_service import UserService
 from .services.token_service import TokenManager
 from .controllers.main_controller import MainController
+# user
+from .repositories.user_repository import UserRepository
+from .services.user_service import UserService
+from .controllers.user_controller import UserController
 # customer
 from .repositories.customer_repository import CustomerRepository
 from .services.customer_service import CustomerService
 from .controllers.customer_controller import CustomerController
+# contract
+from .repositories.contract_repository import ContractRepository
+from .services.contract_service import ContractService
+from .controllers.contract_controller import ContractController
+# event
+from .repositories.event_repository import EventRepository
+from .services.event_service import EventService
+from .controllers.event_controller import EventController
 
 
 def get_user_service(session: Session):
@@ -27,6 +37,18 @@ def get_customer_service(session: Session):
     return CustomerService(session, customer_repository)
 
 
+def get_contract_service(session: Session):
+    """create an instance of ContractService"""
+    contract_repository = ContractRepository(session)
+    return ContractService(session, contract_repository)
+
+
+def get_event_service(session: Session):
+    """create an instance of EventService"""
+    event_repository = EventRepository(session)
+    return EventService(session, event_repository)
+
+
 def get_token_manager() -> TokenManager:
     """create an instance of TokenManager"""
     return TokenManager()
@@ -39,7 +61,26 @@ def init_main_controller(session: Session):
     return MainController(user_service, token_manager)
 
 
+def init_user_controller(session: Session):
+    """Create an instance of UserController"""
+    user_service = get_user_service(session)
+    return UserController(user_service)
+
+
 def init_customer_controller(session: Session):
     """Create an instance of CustomerController"""
     customer_service = get_customer_service(session)
-    return CustomerController(customer_service)
+    user_service = get_user_service(session)
+    return CustomerController(customer_service, user_service)
+
+
+def init_contract_controller(session: Session):
+    """Create an instance of ContractController"""
+    contract_service = get_contract_service(session)
+    return ContractController(contract_service)
+
+
+def init_event_controller(session: Session):
+    """Create an instance of EventController"""
+    event_service = get_event_service(session)
+    return EventController(event_service)
